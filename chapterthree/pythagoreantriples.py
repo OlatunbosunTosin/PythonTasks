d63dfe0cfee948d37e04bf0dcdb5dd285fc2e045
Tosin <adeolamimilabake@gmail.com>
2026-04-18 13:07:38 +0100

    finding pythagorean triples of right triangle

--- chapterthree/pythagoreantriples.py
+++ chapterthree/pythagoreantriples.py
@@ -1,3 +1,18 @@
+"""
+print heading of the triple sides
+for each hypotenuse in range 1 to 21
+    calculate hypothenuse_square = hypothenuse * hypothenuse
+    for each opposite in range 1 to 21
+        calculate pposite_square = opposite * opposite
+        for each adjacent in range1 to 21
+            calculate adjacent_square = adjacent * adjacent
+            calculate sum_of_two_sides = opposite_square + adjacent_square
+            if hypothenuse_square == sum_of_two_sides
+                print hypotenuse, opposite, adjacent
+"""
+
+
+
 print(f"hypothenuse\topposite\tadjacent")
 for hypothenuse in range(1,21):
     hypothenuse_square = hypothenuse * hypothenuse
