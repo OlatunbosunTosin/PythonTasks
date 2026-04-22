377aaffcedb8be0090490b7d5e27b59955623830
Tosin <adeolamimilabake@gmail.com>
2026-04-22 05:57:45 +0100

    printing number triangle

---
+++ tearsOfJoy/numbertriangle.py
@@ -0,0 +1,13 @@
+"""
+for each row from 1 to 6
+    for rach column from row
+        print colum
+    print()
+"""
+
+for row in range(1,6):
+    for column in range(row):
+        print(column, end=" ")
+    print()
+
+
