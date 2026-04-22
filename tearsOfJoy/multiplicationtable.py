c49f7d68ea0b53ddf09065559af12adc76b40eb5
Tosin <adeolamimilabake@gmail.com>
2026-04-22 05:18:43 +0100

    multiplication table for 7

---
+++ tearsOfJoy/multiplicationtable.py
@@ -0,0 +1,10 @@
+"""
+for each number in range 1 to 13
+    multiply 7 by number
+    print multiply
+"""
+
+
+for number in range (1,13):
+    multiply = 7 * number
+    print(f"7 x {number:>2} = {multiply:>2}")
