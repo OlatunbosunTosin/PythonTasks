dc75765fdb5b92c76f14c45e6cd3bd85122d8768
Tosin <adeolamimilabake@gmail.com>
2026-04-22 05:30:44 +0100

    displaying 1 to 10

---
+++ tearsOfJoy/onetotenwhileloop.py
@@ -0,0 +1,12 @@
+"""
+set number to 0
+while number is less than 10
+    increment number by 1
+    print number
+"""
+
+
+number = 0
+while number < 10:
+    number += 1;
+    print(number, end=",")
